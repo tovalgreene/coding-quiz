02b880b9757fcb72ad3dd263a453afb6ccc404b5
Tova Greene <tovalgreene@gmail.com>
2023-06-27 18:01:24 -0400

    Pseudo-coded in JavaScript file

---
+++ script.js
@@ -0,0 +1,14 @@
+//Connect to HTML -- getElementbyID
+
+//Write storage variables (timer, etc)
+
+//Write array for questions
+
+//Add event listeners to start & save quiz
+
+/*Write functions:
+- start the quiz
+- show each question
+- function to check each question (verify the answers)
+- function to end quiz
+- function to save score*/
